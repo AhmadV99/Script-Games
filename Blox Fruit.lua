6e5efa87108ae2ab92d690bfe065a308af5f303a
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2025-01-08 16:42:06 +0400

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Script Temporarily Disabled\n\nReason: Blox Fruit Upgraded Their Anti-Cheat\n\nJoin Our Discord For Any Update: discord.gg/speedhubx")
+game.Players.LocalPlayer:Kick("Script Temporarily Disabled\n\nJoin Our Discord For Any Update: discord.gg/speedhubx")
