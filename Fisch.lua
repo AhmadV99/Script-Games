868b601cab3fc7825b95c99ecaaee0be61f645f0
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-01-17 21:43:46 +0400

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -1,3 +1,3 @@
 local cloneref = cloneref or function(x) return x end
-local Message = Instance.new("Message", gethui() or cloneref(game:GetService("CoreGui")))
+local Message = Instance.new("Message", (gethui() or cloneref(game:GetService("CoreGui"))))
 Message.Text = "The script is paused due to an anti-cheat update. Join the Discord for more info: https://discord.gg/speedhubx"
