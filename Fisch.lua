692bb0e520b7c71b35e5a416cbcd476d60752e77
AhmadV99 <162908247+AhmadV99@users.noreply.github.com>
2026-05-27 00:12:19 +0400

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -1,1 +1,5 @@
+local message = Instance.new("Message", workspace)
 
+message.Text = "This script has been temporarily disabled due to an anti-cheat update.\nPlease join the Discord server for more information and updates:\ndiscord.gg/speedhubx"
+
+setclipboard("discord.gg/speedhubx")
